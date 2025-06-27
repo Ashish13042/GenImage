cf27337312182aa4fac39658f670c792bea74b5a
Ashish13042 <rawatashish0100101@gmail.com>
2025-06-27 10:04:37 +0530

    AppContext updated & login update

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,22 +1,85 @@
-import { createContext, useState } from 'react';
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
+
 
 const AppContextProvider = (props) => {
-    const [user, setUser] = useState(null);
-    const [showLogin, setShowLogin] = useState(false)
+  const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
-    const value = {
-        user,
-        setUser,
-        showLogin,
-        setShowLogin
+  const [credit, setCredit] = useState(false);
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const loadCreditsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/credits", {
+        headers: { token },
+      });
+
+      if (data.success) {
+        setCredit(data.credits);
+        setUser(data.user);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
     }
+  };
 
-    return(
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  const generateImage = async (prompt) => {
+  console.log("Sending userId:", user?._id); // this prints correctly
+
+  const { data } = await axios.post(
+    backendUrl + "/api/image/generate-image",
+    {
+      prompt,
+      userId: user?._id,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json", // 🧠 ensure JSON is enforced
+        token: token,
+      },
+    }
+  );
+
+  return data.image;
+};
+
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    setUser(null);
+  };
+
+  useEffect(() => {
+    if (token) {
+      loadCreditsData();
+    }
+  }, [token]);
+
+  const value = {
+    user,
+    setUser,
+    showLogin,
+    setShowLogin,
+    backendUrl,
+    token,
+    setToken,
+    credit,
+    setCredit,
+    loadCreditsData,
+    logout,
+    generateImage,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
 export default AppContextProvider;
